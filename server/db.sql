a7c785f62fc8bf18d05b40beac43a82ed8bfe5ca
Dasha <dasha.kotova.mail@gmail.com>
2021-05-05 21:53:15 -0400

    add server file and dependencies

---
+++ server/db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE restaurants (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  location VARCHAR(50) NOT NULL,
+  price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)
+);
+
+INSERT INTO restaurants (name, location, price_range) VALUES ('Saigon Lotus', 'Toronto', 3);
